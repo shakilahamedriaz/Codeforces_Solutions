6bf38b5c3eaab2eae811ff768299e3deccc97f60
shakilahamedriaz <shakilahamedriaz@gmail.com>
2022-09-22 02:43:27 +0600

    done

---
+++ 282A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    int n,x=0;
+    string s;
+
+    cin>>n;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>s;
+        if(s[1]== '+')
+        {
+            x++;
+        }
+        else{
+
+            x--;
+        }
+    }
+    cout<<x<<endl;
+
+    return 0;
+}
