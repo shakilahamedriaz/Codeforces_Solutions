823df97122b17a5e691243c2aa2f753c6a9518e6
shakilahamedriaz <shakilahamedriaz@gmail.com>
2022-11-03 01:04:05 +0600

    done

--- 1742A.cpp
+++ 1742A.cpp
@@ -1,25 +1,26 @@
 #include<bits/stdc++.h>
+#include<math.h>
 using namespace std;
 int main(){
 
 int t,a,b,c;
+cin>>t;
 
-for(int i=0; i<t; i++){
+for(int i=1; i<=t; i++){
 
     cin>>a>>b>>c;
 
-    if((a == b + c) || (b == c + a) || (c == a + b)){
+    if(a == b + c || b == a + c || c == a + b){
 
-    cout<<"YES"<<endl;
-
-    }else{
-
-    cout<<"NO"<<endl;
+    cout<< "YES" <<endl;
 
     }
+    else {
+
+        cout<< "NO" <<endl;
+    }
+
 }
-
-
 
 return 0;
 }
