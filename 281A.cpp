1a1465a91084091cc264d8d9d41a694717fad79a
shakilahamedriaz <shakilahamedriaz@gmail.com>
2022-11-04 23:57:43 +0600

    done

---
+++ 281A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+    string a;
+    cin>> a;
+
+    a[0] = towupper(a[0]);
+
+    cout<< a <<endl;
+
+
+
+
+    return 0;
+}
