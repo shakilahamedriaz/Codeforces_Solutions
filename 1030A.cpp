c1fc49bd44f57d78b11a9991f26c42a683ccc861
shakilahamedriaz <shakilahamedriaz@gmail.com>
2022-11-06 22:36:25 +0600

    be strong sr

---
+++ 1030A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+int main(){
+
+int n,t;
+cin>>n;
+
+while(n>0){
+
+    cin>>t;
+
+    if(t==1)
+    {
+        cout<< "HARD"<<endl;
+
+        return 0;
+
+    }
+    n--;
+
+}
+    cout<< "EASY"<<endl;
+
+return 0;
+}
