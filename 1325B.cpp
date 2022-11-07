dbdd176d135ae1e87c7a8ffa31333b27018a809a
shakilahamedriaz <shakilahamedriaz@gmail.com>
2022-11-07 21:00:31 +0600

    done

---
+++ 1325B.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a;
+       int s;
+        while(a--)
+        {
+            int b;
+            cin>>b;
+            s.insert(b);
+        }
+        cout<<s.size()<<endl;
+    }
+    return 0;
+}
