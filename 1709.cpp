a629bcbdd587749718a129f515a305202ae3945b
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-11 23:52:45 +0600

    done

---
+++ 1709.cpp
@@ -0,0 +1,26 @@
+#include <bits\stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+  int t;
+  scanf("%d", &t);
+  while (t--)
+  {
+    int x;
+    int a[3];
+    scanf("%d", &x);
+    scanf("%d %d %d", &a[0], &a[1], &a[2]);
+    int cur = a[x - 1] - 1;
+    int c = 1;
+    while (cur != -1)
+    {
+      cur = a[cur] - 1;
+      c++;
+    }
+    cout << ((c < 3) ? "NO" : "YES") << "\n";
+  }
+}
