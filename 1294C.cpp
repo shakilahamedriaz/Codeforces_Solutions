ea0cafd03cfc07342ad06f4a8896017a7813735e
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-05-08 20:57:53 +0600

    Done

---
+++ 1294C.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void findans(int n)
+{
+    for(int i=2; i*i<n; i++)
+    {
+        if(n%i==0)
+        {
+            int d=n/i;
+
+            for(int j=i+1; j*j<d; j++)
+            {
+                if(d%j==0)
+                {
+                    cout<<i<<" "<<j<<" "<<d/j<<endl;
+                }
+            }
+        }
+    }
+    cout<< "NO"<<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        findans(n);
+    }
+    
+    return 0;
+}
