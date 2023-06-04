703ab72b414f483ef1d50f11a3e06ecf38f1a6a1
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-06-04 15:14:52 +0600

    done

---
+++ 1839A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+    int n, k;
+    cin >> n >> k;
+
+    n=n-2;
+
+    cout<<n/k+2<<endl;
+
+    }
+
+    return 0;
+}
