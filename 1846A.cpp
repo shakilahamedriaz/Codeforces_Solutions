7f14d5be237aa566c5034e74c680f4fa2b5b2bde
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-07-08 17:43:09 +0600

    done

---
+++ 1846A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin>>t;
+    int cnt=0;
+    while(t--)
+    {
+        int a, b;
+        cin>>a>>b;
+
+        if(a>b){
+            cnt++;
+        }
+
+        
+    }
+    cout<<cnt<<endl;
+    return 0;
+}
