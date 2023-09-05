1de30c897a22aec6b11bacba705c68c8b20efea6
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-09-06 02:31:01 +0600

    Done

--- 1807C.cpp
+++ 1807C.cpp
@@ -1,69 +1,47 @@
-#include <iostream>
-#include <string>
-#include <map>
-#include <vector>
+#include<bits/stdc++.h>
+#define ll long long
+#define faster ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 using namespace std;
 
-int main() {
+void solve()
+{
+     int n;
+     cin>>n;
+     string s;
+     cin>>s;
+
+     bool ok=true;
+     map<char, int>last;
+     for(int i=0; i<n; i++)
+     {
+        if(last.find(s[i])==last.end();){
+            last[s[i]]=i;
+        }
+        else {
+            if((i-last[s[i]])%2==1){
+                ok=false;
+                break;
+            } 
+            last[s[i]]=i;
+        }
+     }
+     if(ok){
+        cout<< "Yes"<<endl;
+     }
+     else{
+        cout<< "No"<<endl;
+        
+     }
+}
+
+int main()
+{
+    faster;
     int t;
     cin >> t;
-
-    while (t--) {
-        int n;
-        cin >> n;
-        string str;
-        cin >> str;
-
-        map<char, int> mp;
-        int t = 0;
-        vector<int> v;
-
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                if (mp[str[i]] == 1) {
-                    t = 1;
-                } else {
-                    mp[str[i]] = 0;
-                    v.push_back(0);
-                }
-            } else {
-                if (mp.find(str[i]) != mp.end()) {
-                    if (mp[str[i]] == 0) {
-                        t = 1;
-                    } else {
-                        v.push_back(1);
-                    }
-                } else {
-                    mp[str[i]] = 1;
-                    v.push_back(1);
-                }
-            }
-            if (t == 1) {
-                cout << "NO" << endl;
-                break;
-            }
-        }
-
-        if (t == 0) {
-            for (int i = 0; i < v.size(); i++) {
-                if (i % 2 == 1) {
-                    if (v[i] != 1) {
-                        cout << "NO" << endl;
-                        break;
-                    }
-                } else {
-                    if (v[i] != 0) {
-                        cout << "NO" << endl;
-                        break;
-                    }
-                }
-
-                if (i == v.size() - 1) {
-                    cout << "YES" << endl;
-                }
-            }
-        }
+    while(t--)
+    {
+        solve();
     }
-
     return 0;
 }
