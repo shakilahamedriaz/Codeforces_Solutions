7bf71c35d4cf5331853f851d722dc3c9ab6b6a08
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-09-16 20:44:41 +0600

    ok

---
+++ 1831B.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >>t;
+    while(t--)
+    {
+        int n;
+        cin >>n;
+        int a[n];
+        int b[n];
+        for(int i=0; i<n; i++)
+        {
+            cin >> a[i];
+        }
+        for(int i=0; i<n; i++)
+        {
+            cin >> b[i];
+        }
+        
+    
+        for(int i=0; i<n; i++)
+         {  
+            int cntA=0;
+            if(a[i]==a[i+1]) cntA++;
+            for(int i=0; i<n; i++)
+            {
+              
+            }
+         }
+        
+    }
+}
