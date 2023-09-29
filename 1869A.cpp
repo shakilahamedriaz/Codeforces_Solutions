725c503f232d5e46aad9c23971ccb70b35bdc080
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-09-30 00:15:48 +0600

    div2

--- 1869A.cpp
+++ 1869A.cpp
@@ -1,31 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int MAX = 2e7;
 
-int main() { 
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); 
-    int t;
-    cin >> t;
-    while(t--) {
-        int n;
-        cin >> n;
-        int a[n];
-        for(int i = 0; i < n; i++) {
-            cin >> a[i];
-        }
-        if(n % 2 == 1) {
-            cout << 4 << endl;
-            cout << "1 " << n - 1 << endl;
-            cout << "1 " << n - 1 << endl;
-            cout << n - 1 << " " << n << endl;
-            cout << n - 1 << " " << n << endl;
-            continue;
-        }
-        cout << 8 << endl;
-        for(int i = 1; i <= 8; i++) {
-            cout << "1 " << n << endl;
-        }
+void shakils_sol()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
+
+    if(n % 2 == 0)
+    {
+        cout << 2 << endl;
+        cout << 1 << " " << n << endl;
+        cout << 1 << " " << n << endl;
+    }
+    else {
+        cout << 4 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << n - 1 << " " << n << endl;
+        cout << n - 1 << " " << n << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        shakils_sol();
+    }
+
     return 0;
 }
+
+
